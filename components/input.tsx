a99d99c403fdb9882b9ca8f7bab756e5c8780b64
Ashley Boswell <boswelas@oregonstate.edu>
2024-03-14 16:28:42 -0700

    initial db setup

--- components/input.tsx
+++ components/input.tsx
@@ -11,7 +11,6 @@ export default function Input({
     ...rest
 }: InputProps &
     InputHTMLAttributes<HTMLInputElement>) {
-    console.log(rest);
     return (
         <div className="flex flex-col gap-2">
             <input
