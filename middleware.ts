4c999138cedf1ff90e42a09009ffa015b99b637f
Ashley Boswell <boswelas@oregonstate.edu>
2024-05-05 17:18:16 -0700

    set up middleware

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+    [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+    "/": true,
+    "/login": true,
+    "/sms": true,
+    "/create-account": true,
+};
+
+export async function middleware(request: NextRequest) {
+    const session = await getSession();
+    const exists = publicOnlyUrls[request.nextUrl.pathname];
+    if (!session.id) {
+        if (!exists) {
+            return NextResponse.redirect(new URL("/", request.url));
+        }
+    }
+    else {
+        if (exists) {
+            return NextResponse.redirect(new URL("/profile", request.url));
+        }
+    }
+}
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
