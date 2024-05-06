1639ffc359248c12a6a42147a3e749523595f647
Ashley Boswell <boswelas@oregonstate.edu>
2024-05-05 18:55:57 -0700

    added github login

---
+++ app/github/complete/route.ts
@@ -0,0 +1,65 @@
+import db from "@/lib/database";
+import getSession from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const code = request.nextUrl.searchParams.get("code");
+    if (!code) {
+        return notFound();
+    }
+    const accessTokenParams = new URLSearchParams({
+        client_id: process.env.GITHUB_CLIENT_ID!,
+        client_secret: process.env.GITHUB_CLIENT_SECRET!,
+        code,
+    }).toString();
+    const accessTokenUrl = `https://github.com/login/oauth/access_token?${accessTokenParams}`;
+    const accessTokenResponse = await fetch(accessTokenUrl, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+        },
+    });
+    const { error, access_token } = await accessTokenResponse.json();
+    if (error) {
+        return new Response(null, {
+            status: 400,
+        });
+    }
+    const userProfileResponse = await fetch("https://api.github.com/user", {
+        headers: {
+            Authorization: `Bearer ${access_token}`,
+        },
+        cache: "no-cache",
+    });
+    const { id, avatar_url, login } = await userProfileResponse.json();
+    const user = await db.user.findUnique({
+        where: {
+            github_id: id + ""
+        },
+        select: {
+            id: true
+        }
+    });
+    if (user) {
+        const session = await getSession();
+        session.id = user.id;
+        await session.save();
+        return redirect("/profile");
+    }
+    const newUser = await db.user.create({
+        data: {
+            username: login,
+            github_id: id + "",
+            avatar: avatar_url,
+        },
+        select: {
+            id: true,
+        }
+    });
+    const session = await getSession();
+    session.id = newUser.id;
+    await session.save();
+    return redirect("/profile");
+}
+
---
+++ app/github/start/route.ts
@@ -0,0 +1,12 @@
+
+export function GET() {
+    const baseURL = "https://github.com/login/oauth/authorize";
+    const params = {
+        client_id: process.env.GITHUB_CLIENT_ID!,
+        scope: "read:user, user:email",
+        allow_signup: "true",
+    };
+    const formattedParams = new URLSearchParams(params).toString();
+    const finalURL = `${baseURL}?${formattedParams}`;
+    return Response.redirect(finalURL);
+}
