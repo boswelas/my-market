affc7d5dac175499269555d37ed18626cf067759
Ashley Boswell <boswelas@oregonstate.edu>
2024-05-12 15:13:24 -0700

    added display comments

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 import db from "@/lib/database";
 import LikeButton from "@/components/like-button";
+import CloseButton from "@/components/close-button";
 
 async function getPost(id: number) {
     try {
@@ -71,6 +72,25 @@ function getCachedLikeStatus(postId: number) {
     return cachedOperation(postId);
 }
 
+async function getComments(id: number) {
+    const comments = await db.comment.findMany({
+        where: {
+            postId: id,
+        },
+        select: {
+            payload: true,
+            updated_at: true,
+            user: {
+                select: {
+                    username: true,
+                    avatar: true,
+                }
+            }
+        },
+    });
+    return comments;
+}
+
 export default async function PostDetail({
     params,
 }: {
@@ -86,8 +106,10 @@ export default async function PostDetail({
     }
 
     const { likeCount, isLiked } = await getCachedLikeStatus(id);
+    const comments = await getComments(post.id);
     return (
         <div className="p-5 text-white">
+            <CloseButton />
             <div className="flex items-center gap-2 mb-2">
                 <Image
                     width={28}
@@ -109,8 +131,30 @@ export default async function PostDetail({
                 <div className="flex items-center gap-2 text-neutral-400 text-sm">
                     <EyeIcon className="size-5" />
                     <span>{post.views}</span>
+                    <LikeButton isLiked={isLiked} likeCount={likeCount} postId={id} />
                 </div>
-                <LikeButton isLiked={isLiked} likeCount={likeCount} postId={id} />
+                <div>
+                    <h3 className="text-lg font-semibold mb-2">Comments</h3>
+                    {comments.map((comment, index) => (
+                        <div key={index} className="flex items-center gap-3 mt-5">
+                            <div >
+                                <div className="flex items-center gap-3"> <Image
+                                    width={28}
+                                    height={28}
+                                    className="size-7 rounded-full"
+                                    src={comment.user.avatar!}
+                                    alt={comment.user.username}
+                                />
+                                    <span className="text-xs font-bold ">{comment.user.username}</span>
+                                    <span>·</span>
+                                    <span className="text-xs text-neutral-400">{comment.updated_at.toDateString()}</span>
+                                </div>
+                                <div className="mt-1 ml-10 text-sm">
+                                    <span>{comment.payload}</span></div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
