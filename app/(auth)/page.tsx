f0aa387b6ebbea9d634802abfe05469917b997d2
Ashley Boswell <boswelas@oregonstate.edu>
2024-05-17 11:16:04 -0700

    update icon home

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between min-h-screen p-6">
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
-        <span className="text-9xl">🥕</span>
+        <span className="text-9xl">🛒</span>
         <h1 className="text-4xl">My Market</h1>
         <h2 className="text-2xl">Welcome to Your Market</h2>
       </div>
