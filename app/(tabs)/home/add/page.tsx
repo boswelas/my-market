696098df417a3ec6b48707e62fa26566f5d616e9
Ashley Boswell <boswelas@oregonstate.edu>
2024-05-21 19:20:38 -0700

    refactor add product form

--- app/(tabs)/home/add/page.tsx
+++ app/(tabs)/home/add/page.tsx
@@ -6,6 +6,7 @@ import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import { uploadProduct } from "./actions";
 import { useFormState } from "react-dom";
+import CloseButton from "@/components/close-button";
 
 export default function AddProduct() {
     const [preview, setPreview] = useState("");
@@ -25,7 +26,8 @@ export default function AddProduct() {
 
     return (
         <div>
-            <form action={action} className="p-5 flex flex-col gap-5">
+            <CloseButton />
+            <form action={action} className="p-5 flex flex-col gap-5 max-w-xl mx-auto"  >
                 <label
                     htmlFor="photo"
                     className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover"
@@ -74,6 +76,6 @@ export default function AddProduct() {
                 />
                 <Button text="Upload" />
             </form>
-        </div>
+        </div >
     );
 }
--- components/tab-bar.tsx
+++ components/tab-bar.tsx
@@ -4,14 +4,12 @@ import {
   HomeIcon as SolidHomeIcon,
   NewspaperIcon as SolidNewspaperIcon,
   ChatBubbleOvalLeftEllipsisIcon as SolidChatIcon,
-  VideoCameraIcon as SolidVideoCameraIcon,
   UserIcon as SolidUserIcon,
 } from "@heroicons/react/24/solid";
 import {
   HomeIcon as OutlineHomeIcon,
   NewspaperIcon as OutlineNewspaperIcon,
   ChatBubbleOvalLeftEllipsisIcon as OutlineChatIcon,
-  VideoCameraIcon as OutlineVideoCameraIcon,
   UserIcon as OutlineUserIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
@@ -20,7 +18,7 @@ import { usePathname } from "next/navigation";
 export default function TabBar() {
   const pathname = usePathname();
   return (
-    <div className="fixed bottom-0 left-0 right-0 mx-auto max-w-screen-md grid grid-cols-5 border-neutral-600 border-t px-5 py-3 *:text-white bg-neutral-800">
+    <div className="fixed bottom-0 left-0 right-0 mx-auto max-w-screen-md grid grid-cols-4 border-neutral-600 border-t px-5 py-3 *:text-white bg-neutral-800">
       <Link href="/home" className="flex flex-col items-center gap-px">
         {pathname === "/home" ? (
           <SolidHomeIcon className="w-7 h-7" />
@@ -44,14 +42,6 @@ export default function TabBar() {
           <OutlineChatIcon className="w-7 h-7" />
         )}
         <span>Chat</span>
-      </Link>
-      <Link href="/live" className="flex flex-col items-center gap-px">
-        {pathname === "/live" ? (
-          <SolidVideoCameraIcon className="w-7 h-7" />
-        ) : (
-          <OutlineVideoCameraIcon className="w-7 h-7" />
-        )}
-        <span>Shop</span>
       </Link>
       <Link href="/profile" className="flex flex-col items-center gap-px">
         {pathname === "/profile" ? (
