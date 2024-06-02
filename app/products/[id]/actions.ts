7d478a50172a98e4acb132213573ea1348a12e99
Ashley Boswell <boswelas@oregonstate.edu>
2024-06-01 18:39:05 -0700

    update chat from prod[id]

--- app/products/[id]/actions.ts
+++ app/products/[id]/actions.ts
@@ -34,4 +34,44 @@ export default async function deleteProduct(productId: number) {
         return notFound();
     }
 }
+export async function checkExistingChat(userId: number, owner: number, productId: number) {
+    try {
+        const isChat = await db.chatRoom.findFirst({
+            where: {
+                AND: [
+                    { users: { some: { id: userId } } },
+                    { users: { some: { id: owner } } },
+                    { productId: productId }
+                ],
+            },
+            select: { id: true },
+        });
+        return isChat ? isChat.id : null;
+    } catch (error) {
+        console.error('Error checking existing chat:', error);
+        throw error;
+    }
+};
 
+export async function createChatRoom(userId: number, owner: number, productId: number) {
+    try {
+        const room = await db.chatRoom.create({
+            data: {
+                product: {
+                    connect: {
+                        id: productId,
+                    }
+                },
+                users: {
+                    connect: [{ id: userId }, { id: owner }],
+                },
+            },
+            select: { id: true },
+        });
+
+        return room.id;
+    } catch (error) {
+        console.error('Error creating chat room:', error);
+        throw error;
+    }
+};
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -6,6 +6,7 @@ import { formatToDollar } from "@/lib/utils";
 import { UserIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { notFound, redirect } from "next/navigation";
+import { checkExistingChat, createChatRoom } from "./actions";
 
 async function getIsOwner(userId: number) {
     const session = await getSession();
@@ -56,25 +57,14 @@ export default async function ProductDetail({
         return notFound();
     }
     const isOwner = await getIsOwner(product.userId);
-    const createChatRoom = async () => {
+    const getChat = async () => {
         "use server";
         const session = await getSession();
-        const room = await db.chatRoom.create({
-            data: {
-                users: {
-                    connect: [{
-                        id: product.userId
-                    }, {
-
-                        id: session.id
-                    },],
-                },
-            },
-            select: {
-                id: true,
-            },
-        });
-        redirect(`../chats/${room.id}`)
+        let room = await checkExistingChat(session.id!, product.userId, product.id);
+        if(!room){
+            createChatRoom(session.id!, product.userId, product.id)
+        }
+        redirect(`../chats/${room}`)
     }
 
     const editProduct = async () => {
@@ -89,44 +79,44 @@ export default async function ProductDetail({
             </div>
             <div className="">
                 <div className="relative aspect-square h-96">
-                <Image fill src={product.photo} alt={product.title} className="object-cover" />
-            </div>
-            <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
-                <div className="size-10 rounded-full overflow-hidden">
-                    {product.user.avatar !== null ? (
-                        <Image
-                            src={product.user.avatar}
-                            width={40}
-                            height={40}
-                            alt={product.user.username}
-                        />
-                    ) : (
-                        <UserIcon />
-                    )}
+                    <Image fill src={product.photo} alt={product.title} className="object-cover" />
                 </div>
-                <div>
-                    <h3>{product.user.username}</h3>
+                <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
+                    <div className="size-10 rounded-full overflow-hidden">
+                        {product.user.avatar !== null ? (
+                            <Image
+                                src={product.user.avatar}
+                                width={40}
+                                height={40}
+                                alt={product.user.username}
+                            />
+                        ) : (
+                            <UserIcon />
+                        )}
+                    </div>
+                    <div>
+                        <h3>{product.user.username}</h3>
+                    </div>
                 </div>
-            </div>
-            <div className="p-5">
-                <h1 className="text-2xl font-semibold">{product.title}</h1>
-                <p>{product.description}</p>
-            </div>
+                <div className="p-5">
+                    <h1 className="text-2xl font-semibold">{product.title}</h1>
+                    <p>{product.description}</p>
+                </div>
                 {isOwner ? (
                     <div className="fixed bottom-0 left-0 right-0 mx-auto max-w-screen-md grid grid-cols-3 border-neutral-600 border-t px-5 py-3 *:text-white bg-neutral-800">
-                <span className="font-semibold text-xl">
-                    ${formatToDollar(product.price)}
-                </span>
-                    <form action={editProduct}>
-                        <button className="bg-blue-500 px-5 py-2.5 rounded-md text-white font-semibold">
-                            Edit product
-                        </button></form>
-                    <DeleteProductModal productId={product.id} />
+                        <span className="font-semibold text-xl">
+                            ${formatToDollar(product.price)}
+                        </span>
+                        <form action={editProduct}>
+                            <button className="bg-blue-500 px-5 py-2.5 rounded-md text-white font-semibold">
+                                Edit product
+                            </button></form>
+                        <DeleteProductModal productId={product.id} />
                     </div>
                 ) : <div className="fixed bottom-0 left-0 right-0 mx-auto max-w-screen-md grid grid-cols-2 border-neutral-600 border-t px-5 py-3 *:text-white bg-neutral-800">
                     <span className="font-semibold text-xl flex items-center pl-8">
                         ${formatToDollar(product.price)}
-                    </span> <form action={createChatRoom} className="flex items-center pl-64">
+                    </span> <form action={getChat} className="flex items-center pl-64">
                         <button
                             className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold flex items-center">
                             Chat
