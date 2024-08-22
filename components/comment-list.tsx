6087af4fd8fe26ff127c94161c32caafdb2169be
Ashley Boswell <boswelas@oregonstate.edu>
2024-08-22 14:33:44 -0700

    improved comment section formatting

--- components/comment-list.tsx
+++ components/comment-list.tsx
@@ -18,30 +18,34 @@ export default function CommentList({ comments }: CommentListProps) {
 
     return (
         <div>
-            <h3 className="text-xl font-semibold mt-1 mb-1">Comments</h3>
-            {comments.map((comment, index) => (
-                <div key={index} className="flex items-center gap-3 mt-5">
-                    <div>
-                        <div className="flex items-center gap-3">
-                            {comment.user.avatar && (
-                                <Image
-                                    width={28}
-                                    height={28}
-                                    className="size-7 rounded-full"
-                                    src={comment.user.avatar}
-                                    alt={comment.user.username}
-                                />
-                            )}
-                            <span className="text-xs font-bold">{comment.user.username}</span>
-                            <span>·</span>
-                            <span className="text-xs text-neutral-400">{GetPostTime(comment.updated_at)}</span>
-                        </div>
-                        <div className="ml-10">
-                            <span>{comment.payload}</span>
+            <div className="mt-10 mb-2">
+                <h3 className="text-xl font-semibold">Comments</h3>
+            </div>
+            <div>
+                {comments.map((comment, index) => (
+                    <div key={index} className="flex items-center gap-3 mb-5">
+                        <div>
+                            <div className="flex items-center gap-3">
+                                {comment.user.avatar && (
+                                    <Image
+                                        width={28}
+                                        height={28}
+                                        className="size-7 rounded-full"
+                                        src={comment.user.avatar}
+                                        alt={comment.user.username}
+                                    />
+                                )}
+                                <span className="text-xs font-bold">{comment.user.username}</span>
+                                <span>·</span>
+                                <span className="text-xs text-neutral-400">{GetPostTime(comment.updated_at)}</span>
+                            </div>
+                            <div className="ml-10">
+                                <span>{comment.payload}</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
