f04069e476a1e68e9a3308a1809120ce1da78f96
Ashley Boswell <boswelas@oregonstate.edu>
2024-08-22 15:49:01 -0700

    updated login image

--- app/(auth)/home-login/page.tsx
+++ app/(auth)/home-login/page.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import "@/lib/database";
+import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between min-h-screen mt-[10%]">
       <div className="max-w-screen-md">
         <div className="my-auto flex flex-col items-center gap-3 *:font-medium">
-          <span className="text-9xl">🛒</span>
+          <Image src={"/shopping_bags.png"} width={50}
+            height={50} alt="" />
+          <span className="text-9xl"></span>
           <h1 className="text-4xl">My Market</h1>
           <h2 className="text-2xl">Welcome to Your Market</h2>
         </div>
