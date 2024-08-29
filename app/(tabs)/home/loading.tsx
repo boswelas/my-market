bdb60f5f5f2a0f811ede559b63b9b7b0133a170b
Ashley Boswell <boswelas@oregonstate.edu>
2024-08-29 11:08:59 -0700

    updated loading screen to reflect style changes

--- app/(tabs)/home/loading.tsx
+++ app/(tabs)/home/loading.tsx
@@ -1,16 +1,22 @@
 export default function Loading() {
     return (
-        <div className="p-5 animate-pulse flex flex-col gap-5">
-            {[...Array(10)].map((_, index) => (
-                <div key={index} className="*:rounded-md flex gap-5 animate-pulse">
-                    <div className="size-28 bg-neutral-700" />
-                    <div className="flex flex-col gap-2 *:rounded-md">
-                        <div className="bg-neutral-700 h-5 w-40" />
-                        <div className="bg-neutral-700 h-5 w-20" />
-                        <div className="bg-neutral-700 h-5 w-10" />
+        <div className="p-5 animate-pulse flex justify-center gap-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+
+                {[...Array(8)].map((_, index) => (
+                    <div key={index} className="flex md:flex-col bg-neutral-800 rounded-lg w-80 sm:w-96 md:w-[17em] md:h-96 shadow-xl hover:shadow-2xl">
+                        <div className="flex items-center justify-center md:w-full md:h-80 md:mt-[3%]">
+                            <div className="relative bg-neutral-700 md:w-60 md:h-72 size-28 rounded-lg" />
+                        </div>
+                        <div className="flex flex-col items-center justify-center">
+                            <div className="flex flex-col ml-5 md:ml-0 bg-neutral-700 justify-center rounded-lg p-2 w-[10em] md:w-[70%] mb-1" />
+                            <div className="flex flex-col ml-5 md:ml-0 bg-neutral-700 justify-center rounded-lg p-2 w-[5em] md:w-[60%] mb-1" />
+                            <div className="flex flex-col ml-5 md:ml-0 bg-neutral-700 justify-center rounded-lg p-2 w-[2em] md:w-[50%] mb-1" />
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
+
     );
 }
